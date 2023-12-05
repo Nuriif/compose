2ebc259f83a2eef6d96be2920301c855a87f7497
박인정(Jay) <86592396+InJungPark@users.noreply.github.com>
2023-12-05 17:20:23 +0900

    css, html 수정 및 경로 변경된 사항 반영 pull 필수!

---
+++ public/js/jquery_header.js
@@ -0,0 +1,8 @@
+//gnb bg
+$(document).ready(function(){
+      $(".gnb, .gnb_bg").hover(function(){
+         $(".gnb ul, .gnb_bg").stop().slideDown(800);
+      },function(){
+         $(".gnb ul, .gnb_bg").stop().slideUp(10);
+      })
+     })
