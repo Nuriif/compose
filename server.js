39597c2d81ac56f395731a8d87fcfae646bdd4b7
박인정(Jay) <86592396+InJungPark@users.noreply.github.com>
2023-12-05 12:21:38 +0900

    node 설정 작업 commit & push

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-/** 라이브러리 import */
+/* 라이브러리 import */
 const http = require("http");
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
 
 var app = express();
-/**
-* 정적 리소스 제공
-* 이렇게 작성하면 html에서 css, js 등의 정적 리소스를 불러올 때 public 폴더를 기준으로 검색 수행
-ex) 만약 notice.css 파일을 불러오고자 하는 경우 다음과 같이 작성
-<link rel="stylesheet" href="/css/notice.css">
+/*
+  정적 리소스 제공
+  이렇게 작성하면 html에서 css, js 등의 정적 리소스를 불러올 때 public 폴더를 기준으로 검색 수행
+  ex) 만약 notice.css 파일을 불러오고자 하는 경우 다음과 같이 작성
+  <link rel="stylesheet" href="/css/notice.css">
 */
 app.use(express.static(path.join(__dirname, 'public')));
 
-/**
- * url 연결 목록 start
- */
+/*
+  url 연결 목록 start
+*/
 app.use('/', express.static(path.join(__dirname, 'html')));
 app.use('/findStore', express.static(path.join(__dirname, '/html/store.html')));
 app.use('/noticeMain', express.static(path.join(__dirname, '/html/noticeList.html')));
-/**
- * url 연결 목록 end
- */
+/*
+  url 연결 목록 end
+*/
 
 // json 데이터 조회
 app.get('/data', (req, res) => {
