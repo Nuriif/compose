b0631e86ddc839622f5f5238d0fbb478f31b7be5
Nammoonee <bimil323@naver.com>
2023-12-22 17:16:40 +0900

    max-widh id추가

---
+++ public/js/customer.js
@@ -0,0 +1,12 @@
+$(function () {
+    $(".tab_content").hide();
+    $(".tab_content:first").show();
+
+    $("ul.tabs li").click(function () {
+        $("ul.tabs li").removeClass("active").css("color", "#555");
+        $(this).addClass("active").css("color", "#fff");
+        $(".tab_content").hide()
+        var activeTab = $(this).attr("rel");
+        $("#" + activeTab).fadeIn()
+    });
+});
